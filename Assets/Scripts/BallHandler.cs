8a5574259ad49de1677602c56dbfb74ebf3771a4
BarrusRho <bargee87@googlemail.com>
2022-07-23 19:43:14 +0100

    Can move ball via manipulating the physics system kinematics

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -7,6 +7,9 @@ namespace MobileGameDev.BallHandler
     {
         private Camera _mainCamera;
 
+        [SerializeField]
+        private Rigidbody2D _ballRigidbody;
+
         private void Start()
         {
             _mainCamera = Camera.main;
@@ -15,13 +18,19 @@ namespace MobileGameDev.BallHandler
         {
             if (!Touchscreen.current.primaryTouch.press.isPressed)
             {
+                _ballRigidbody.isKinematic = false;
+
                 return;
             }
             else
             {
+                _ballRigidbody.isKinematic = true;
+
                 Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
                 Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+
+                _ballRigidbody.position = worldPosition;
 
                 Debug.Log($"{worldPosition}");
             }
