c3cc11950c6adb4c2ba5fb6ad227940450f80111
BarrusRho <bargee87@googlemail.com>
2022-07-23 19:32:02 +0100

    Converted screen position to world position

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,6 +5,12 @@ namespace MobileGameDev.BallHandler
 {
     public class BallHandler : MonoBehaviour
     {
+        private Camera _mainCamera;
+
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+        }
         private void Update()
         {
             if (!Touchscreen.current.primaryTouch.press.isPressed)
@@ -15,7 +21,9 @@ namespace MobileGameDev.BallHandler
             {
                 Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-                Debug.Log($"{touchPosition}");
+                Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+
+                Debug.Log($"{worldPosition}");
             }
         }
     }
